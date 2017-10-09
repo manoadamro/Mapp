ebd29926775626040a7a2309613a58da083465be
Adam Romano <manoadamro.imp@gmail.com>
2017-10-09 13:25:12 +0100

    added doc strings to controller files and lib/server

--- controllers/message.py
+++ controllers/message.py
@@ -5,3 +5,25 @@ class Message:
     def __init__(self):
         pass
 
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def new(self, **params):
+
+        """
+        for sending a new message
+        :param params: POST params
+        :return: json response
+        """
+        pass
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def update(self, **params):
+
+        """
+        for updating received messages
+        :param params: GET params
+        :return: json response
+        """
+
+        pass
--- controllers/query.py
+++ controllers/query.py
@@ -2,5 +2,26 @@ import cherrypy
 
 
 class Query:
+
     def __init__(self):
+        pass
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def online(self, **params):
+
+        """
+        gets data for all online users and packages it into json object
+        :param params: GET params
+        :return: a json object containing all online users
+        """
+        pass
+
+    def find(self, **params):
+
+        """
+        gets single user by specified search field and packages into json object
+        :param params: GET params
+        :return: a json object containing single user if found or response if not
+        """
         pass
