c4c7a17730435769798c2e63fc7d2410a55ab166
Adam Romano <manoadamro.imp@gmail.com>
2017-10-09 17:41:33 +0100

    added MessageLog

--- lib/message_log/__init__.py
+++ lib/message_log/__init__.py
@@ -1,16 +1,25 @@
+from time import time
 
 
 class MessageLog:
     def __index__(self):
         self.message_list = []
 
-    def add_message(self):
+    def add_message(self, text, author):
+        message = self._new_message(text, author)
+        self.message_list.append(message)
 
-        message = {
+    def get_messages(self, index):
+        if len(self.message_list) > index:
+            next_index = index + 1
+            return self.message_list[next_index:]
+        else:
+            return []
+
+    def _new_message(self, text, author):
+        return {
             'index': len(self.message_list),
-            'test': ''
+            'text': text,
+            'author': author,
+            'timestamp': time()
         }
-        pass
-
-    def get_messages(self):
-        pass
--- lib/message_log/test_messageLog.py
+++ lib/message_log/test_messageLog.py
@@ -2,4 +2,8 @@ from unittest import TestCase
 
 
 class TestMessageLog(TestCase):
-    pass
+    def test_add_message(self):
+        self.assertTrue(True)
+
+    def test_get_messages(self):
+        self.assertTrue(True)
