ccad99236bab46da881b3966caf5bd7232e633ad
Adam Romano <manoadamro.imp@gmail.com>
2017-10-09 14:00:20 +0100

    setup testing framework

---
+++ lib/server/test_server.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from . import Server
+
+
+class TestServer(TestCase):
+    def test_build(self):
+        self.fail()
+
+    def test_start(self):
+        self.fail()
