b4bba9febd194a0c6b90d72dc8434560b39115f9
Adam Romano <manoadamro.imp@gmail.com>
2017-10-10 16:25:10 +0100

    changed port to 80

--- scripts/server.py
+++ scripts/server.py
@@ -28,6 +28,8 @@ class Server:
         Build the routes from the config dictionary passed into constructor
         :return: no return value
         """
+        cherrypy.config.update({'server.socket_port': 80})
+
         for item in self.site_map:
             app = self.site_map[item]()
             self.tree.mount(app, item, self.app_config())
