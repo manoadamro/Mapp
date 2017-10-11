78d5c201961d370b63f97502298cd39399829d52
Adam Romano <manoadamro.imp@gmail.com>
2017-10-11 13:20:46 +0100

    added temporary comments to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 var index = -1
 var channel = 'global'
 
+
+// New Message
 $("#send").click(function(event) {
 	message = document.getElementById('messageForm').value;
 	params = {"message": message, "channel": channel}
@@ -9,6 +11,8 @@ $("#send").click(function(event) {
 	event.preventDefault();
 });
 
+
+// Get Updates
 var getUpdates = function() {
     params = {"index": index, "channel": channel}
     $.get("/chat/update", params).done(function(response){
@@ -16,6 +20,7 @@ var getUpdates = function() {
             var parsedMessages = response.data;
             if (parsedMessages.length > 0) {
                 renderHTML(parsedMessages);
+                index = parsedMessages[parsedMessages.length - 1].index
             }
         }
         else {
@@ -25,33 +30,48 @@ var getUpdates = function() {
 };
 
 
+// Create Channel
 var createChannel = function() {
     // route -> '/chat/create'
+    // params-> 'channel' (name of channel)
 }
 
+
+// Delete Channel
 var deleteChannel = function() {
     // route -> '/chat/delete'
+    // params-> 'channel' (name of channel)
 }
 
+
+// Join Channel
 var joinChannel = function() {
     // route -> '/chat/join'
+    // params-> 'channel' (name of channel)
+
+    // set channel var to channel name if response code is 0
 }
 
+
+// Leave Channel
 var leaveChannel = function() {
     // route -> '/chat/leave'
+    // params-> 'channel' (name of channel)
+
+    // set channel var to [something] if response code is 0
 }
 
 
+// Render Messages As HTML
 var renderHTML = function(data) {
     var htmlString = '';
     for (i = 0; i < data.length; i++) {
         htmlString += (data[i].author + ': ' + data[i].text + '<br/>')
     }
     document.getElementById("messageList").innerHTML += htmlString;
-    index = data[data.length - 1].index
 }
 
-
+// Update Loop
 var timeout = function(){
 setTimeout(function () {
     getUpdates();
@@ -60,6 +80,8 @@ setTimeout(function () {
 }
 
 
+// Force Login So We Can Use It
 $.post("/session/login", {'username': 'monkey'})
 
+// Begin Update Loop
 timeout();
