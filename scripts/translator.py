53958870bf822df1541b671c25b92241bbc14185
stephengeller <stephen.geller@hotmail.com>
2017-10-12 15:51:39 +0100

    formatted api key to environ vars

--- scripts/translator.py
+++ scripts/translator.py
@@ -2,9 +2,24 @@
 
 from google.cloud import translate
 from os import getcwd, environ
-from json import loads
+from json import loads, dumps
+path = "%s/config/google.json" % getcwd()
 
-print(environ['GOOGLE_APPLICATION_CREDENTIALS'])
+file = open(path, 'r')
+json = loads(file.read())
+file.close()
+
+print(json)
+
+json['private_key_id'] = environ["PRIVATE_KEY_ID_TRANSLATOR"]
+json['private_key'] = environ["PRIVATE_KEY_TRANSLATOR"]
+
+file = open(path, 'w')
+file.write(dumps(json))
+file.close()
+
+print(json)
+
 class Translator:
     def __init__(self):
         self.translate_client = translate.Client.from_service_account_json(environ['GOOGLE_APPLICATION_CREDENTIALS'])
