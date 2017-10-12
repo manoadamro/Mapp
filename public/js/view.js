d0ff0ef4ba6b0b334326a2c9df7e4d3f7a033fea
Adam Romano <manoadamro.imp@gmail.com>
2017-10-12 12:51:47 +0100

    refactored javascript

---
+++ public/js/view.js
@@ -0,0 +1,69 @@
+var view = '';
+
+
+var setChannelView = function(){
+
+    document.getElementById('page').innerHTML = languageSelector + messageList + messageForm
+    view = 'channel'
+
+    $("#setLanguage").click(function(event) {
+        changeLanguage();
+        event.preventDefault();
+    });
+
+    $("#send").click(function(event) {
+        message = document.getElementById('messageForm').value;
+        params = {"message": message, "channel": channel}
+        $.post("/chat/message", params).done(function(response){
+            if (response.code === 0){
+                document.getElementById('messageForm').value = '' 
+                displayError('')
+            }
+        })
+        event.preventDefault();
+    });
+
+    $("#logout").click(function(event) {
+        clearSession();
+    });
+
+    $("#addChannel").click(function(event) {
+        name = document.getElementById('channelForm').value
+        createChannel(name);
+        event.preventDefault();
+    });
+
+    $("#deleteChannel").click(function(event) {
+        deleteChannel(channel);
+        event.preventDefault();
+    });
+
+    refreshTargetLanguage(targetLanguage)
+    updateLoop();
+}
+
+
+var setLogInView = function() {
+
+    document.getElementById('page').innerHTML = loginForm
+    view = 'login'
+
+    $("#login").click(function(event) {
+        var username = document.getElementById('usernameForm').value
+        if (username.length === 0) {
+            displayError('Username can not be empty')
+        }
+        else {
+            $.post("/session/login", {'username': username, 'language': 'en'}).done(function(response){
+                if (response.code == 0) {
+                    setChannelView();
+                    displayError('')
+                }
+                else {
+                    displayError(response.message);
+                }
+            });
+        }
+        event.preventDefault();
+    });
+}
