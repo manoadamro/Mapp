d9a23461d7a58d4f472011a0fb18cf19e6a4b4ce
stephengeller <stephen.geller@hotmail.com>
2017-10-13 10:48:04 +0100

    added logic for stopping retention of private google keys

---
+++ scripts/private_key_adder.py
@@ -0,0 +1,29 @@
+from os import getcwd, environ
+from json import loads, dumps, dump
+
+class PrivateKeyAdder:
+
+    def __init__(self, path, original_key_path):
+        self.path = path
+        self.original_key_path = original_key_path
+
+    path = "%s/config/key.json" % getcwd()
+
+    def add_google_key_to_json_file(self):
+        file = open(self.path, 'r')
+        json = loads(file.read())
+        file.close()
+        json["private_key_id"] = environ["PRIVATE_KEY_ID_TRANSLATOR"]
+        json["private_key"] = environ["PRIVATE_KEY_TRANSLATOR"]
+        file = open(self.path, 'w')
+        file.write(dumps(json, separators=(',',':')))
+        file.close()
+
+    def reset_key(self):
+        file = open(self.original_key_path, 'r')
+        original_key_info = loads(file.read())
+        print("\n\n\n\n%s\n\n\n\n" % original_key_info)
+        file.close()
+        file = open(self.path, 'w')
+        file.write(dumps(original_key_info, separators=(',',':')))
+        file.close()
