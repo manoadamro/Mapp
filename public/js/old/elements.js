89f5762f2ec7e7897123fb05aceef34e005e3e4a
Adam Romano <manoadamro.imp@gmail.com>
2017-10-16 20:25:59 +0100

    now uses nothing from /old

--- public/js/old/elements.js
+++ public/js/old/elements.js
@@ -1,27 +1,2 @@
 
-var messageList = '<div id="messageList" class="message-list"></div>';
 
-var messageForm =
-	'<form class="message-form">' +
-	'<input class="text-box" type="text" type="textarea" id="messageForm"></input>' +
-	"<br />" +
-	'<button class="btn btn-primary" id="send">Send</button>' +
-	"</form>" +
-	'<form class="center">' +
-	"<br />" +
-	'<button class="btn btn-danger" id="logout">Log Out</button>' +
-	"</form>";
-
-var loginForm =
-	"<form>" +
-	"<div class='name-input'>Input your name:<div>" +
-	'<input class="usernameForm" type="text" id="usernameForm"></input>' +
-	"<br />" +
-	'<button class="login btn btn-success" id="login">Log In</button>' +
-	"</form>";
-
-var createChannel =
-	'<form id="createChannel">' +
-	'<input class="" type="text" id="channelForm"></input>' +
-	"<button>Create Channel</button>" +
-	"</form>";
--- public/js/render.js
+++ public/js/render.js
@@ -6,6 +6,14 @@ var view = "";
 
 var renderLogInView = function(){
 
+	var loginForm =
+		"<form>" +
+		"<div class='name-input'>Input your name:<div>" +
+		'<input class="usernameForm" type="text" id="usernameForm"></input>' +
+		"<br />" +
+		'<button class="login btn btn-success" id="login">Log In</button>' +
+		"</form>";
+
 	session.logOut(null);
     document.getElementById("page").innerHTML = loginForm;
 	view = "login";
@@ -29,6 +37,25 @@ var renderLogInView = function(){
 }
 
 var renderChannelView = function(){
+
+	var messageList = '<div id="messageList" class="message-list"></div>';
+
+	var messageForm =
+		'<form class="message-form">' +
+		'<input class="text-box" type="text" type="textarea" id="messageForm"></input>' +
+		"<br />" +
+		'<button class="btn btn-primary" id="send">Send</button>' +
+		"</form>" +
+		'<form class="center">' +
+		"<br />" +
+		'<button class="btn btn-danger" id="logout">Log Out</button>' +
+		"</form>";
+
+	var createChannel =
+		'<form id="createChannel">' +
+		'<input class="" type="text" id="channelForm"></input>' +
+		"<button>Create Channel</button>" +
+		"</form>";
 
     document.getElementById("page").innerHTML = languageList + messageList + messageForm;
 	view = "channel";
